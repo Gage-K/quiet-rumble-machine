e73008f1807b0b3707fa71cc1c06b77a7cf80544
viola he <viola@nyu.edu>
2025-07-07 22:42:27 -0400

    added 2D array function

--- app.js
+++ app.js
@@ -10,40 +10,49 @@ qr.make();
 console.log(qr.getModuleCount()); // 177 for 40, pixels inone direction
 // document.getElementById('placeHolder').innerHTML = qr.createImgTag(); // print qr code for reference
 
+const size = qr.getModuleCount();
 
-// goes down each column for moduleCount amounts of pixels, then across the row
+// 1D array - goes down each column for moduleCount amounts of pixels, then across the row
 function getPixels(qr) {
     let qrPixels = [];
-    for (let i = 0; i < qr.getModuleCount(); i++) { // colums
-        for (let j = 0; j < qr.getModuleCount(); j++) { // rows?
+    for (let i = 0; i < size; i++) { // colums
+        for (let j = 0; j < size; j++) { // rows?
             qrPixels.push(qr.isDark(j, i));
         }
     }
     return qrPixels;
 }
 
+// 2d array! [row][col]
+function getPixels2D(qr) {
+    const qrPixels2D = [];
+    for (let row = 0; row < size; row++) {
+        qrPixels2D[row] = [];
+        for (let col = 0; col < size; col++) {
+            qrPixels2D[row][col] = qr.isDark(row, col);
+        }
+    }
+    return qrPixels2D;
+}
+
 let qrPixels = getPixels(qr);
+let qrPixels2D = getPixels2D(qr);
+console.log(qrPixels2D);
+console.table(qrPixels);
+
 
 const container = document.getElementById('container');
 
 const pixelElement = document.createElement('div');
 
 for (let i = 0; i < qr.getModuleCount() * qr.getModuleCount(); i++) {
-    const pixelElement = document.createElement('button');
-    console.log(qrPixels[i]);
-    (qrPixels[i] === true) ? pixelElement.classList.add('black') : pixelElement.classList.add('white');
-    container.appendChild(pixelElement);
+    for (let j = 0; j < qr.getModuleCount(); j++) {
+        const pixelElement = document.createElement('button');
+        // console.log(qrPixels[i][j]);
+        (qrPixels2D[i][j] === true) ? pixelElement.classList.add('black') : pixelElement.classList.add('white');
+        container.appendChild(pixelElement);
+    }
 }
 
 
-
-console.table(getPixels(qr));
-
-
-
-
-
-
-// qr.addData('Hi!');
-// qr.make();
-// document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+// console.table(getPixels(qr));

586193987d8af2e1a27c2b06c9a56f0e1be8d29c
Gage-K <krausegage@gmail.com>
2025-07-08 10:49:10 -0400

    Clean up 2d array traversal logic and incorporate config

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-console.log("hello world, ready to rumble");
+import config from "./config/qr-config.js";
 
-var typeNumber = 4; // size or version -- from 1 (21) to 40 (177)
-var errorCorrectionLevel = 'H';
-var qr = qrcode(typeNumber, errorCorrectionLevel);
+var qr = qrcode(config.typeNumber, config.errorCorrection);
 
-qr.addData('https://www.google.com');
+qr.addData("https://www.google.com");
 qr.make();
 
 console.log(qr.getModuleCount()); // 177 for 40, pixels inone direction
@@ -14,25 +12,27 @@ const size = qr.getModuleCount();
 
 // 1D array - goes down each column for moduleCount amounts of pixels, then across the row
 function getPixels(qr) {
-    let qrPixels = [];
-    for (let i = 0; i < size; i++) { // colums
-        for (let j = 0; j < size; j++) { // rows?
-            qrPixels.push(qr.isDark(j, i));
-        }
+  let qrPixels = [];
+  for (let i = 0; i < size; i++) {
+    // colums
+    for (let j = 0; j < size; j++) {
+      // rows?
+      qrPixels.push(qr.isDark(j, i));
     }
-    return qrPixels;
+  }
+  return qrPixels;
 }
 
 // 2d array! [row][col]
 function getPixels2D(qr) {
-    const qrPixels2D = [];
-    for (let row = 0; row < size; row++) {
-        qrPixels2D[row] = [];
-        for (let col = 0; col < size; col++) {
-            qrPixels2D[row][col] = qr.isDark(row, col);
-        }
+  const qrPixels2D = [];
+  for (let row = 0; row < size; row++) {
+    qrPixels2D[row] = [];
+    for (let col = 0; col < size; col++) {
+      qrPixels2D[row][col] = qr.isDark(row, col);
     }
-    return qrPixels2D;
+  }
+  return qrPixels2D;
 }
 
 let qrPixels = getPixels(qr);
@@ -40,19 +40,18 @@ let qrPixels2D = getPixels2D(qr);
 console.log(qrPixels2D);
 console.table(qrPixels);
 
+const container = document.getElementById("container");
 
-const container = document.getElementById('container');
+const pixelElement = document.createElement("div");
 
-const pixelElement = document.createElement('div');
-
-for (let i = 0; i < qr.getModuleCount() * qr.getModuleCount(); i++) {
-    for (let j = 0; j < qr.getModuleCount(); j++) {
-        const pixelElement = document.createElement('button');
-        // console.log(qrPixels[i][j]);
-        (qrPixels2D[i][j] === true) ? pixelElement.classList.add('black') : pixelElement.classList.add('white');
-        container.appendChild(pixelElement);
-    }
+for (let row = 0; row < size; row++) {
+  for (let col = 0; col < size; col++) {
+    const pixelElement = document.createElement("button");
+    qrPixels2D[row][col] === true
+      ? pixelElement.classList.add("black")
+      : pixelElement.classList.add("white");
+    container.appendChild(pixelElement);
+  }
 }
-
 
 // console.table(getPixels(qr));

27ec54dfe902d7194389eaf28846dc8cf3dba906
Gage-K <krausegage@gmail.com>
2025-07-14 15:46:22 -0400

    Update config to use some constants for track and step counts - add default state to config

--- config/qr-config.js
+++ config/qr-config.js
@@ -1,7 +1,13 @@
+const tracks = 4;
+const steps = 16;
+
 const config = {
   typeNumber: 4, // Versions 1 (21 nodes) thtrough 40 (177 nodes)
   errorCorrection: "H",
   baseUrl: "www.google.com",
+  tracks,
+  steps,
+  defaultState: Array(tracks).fill(Array(steps).fill(false)),
 };
 
 export default config;
